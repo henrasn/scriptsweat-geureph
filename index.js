2d92a990a4adf7226785a0df029d5e81b2e5f8ea
Henra Setia Nugraha <henra.sn@gmail.com>
2016-07-30 01:47:30 +0700

    try fix produk 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var Schema = require('./graph');
 // var query = 'query{showSingleData(id:1234){completed,title}}';
 // var query = 'mutation mutationRate{add(idProduk:"32423"){}}';
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 mongoose.connect('mongodb://dbrest:restdatabase@ds031965.mlab.com:31965/dbscript');
 // mongoose.connect('mongodb://127.0.0.1:27017/produk');
 // mongoose.connect('mongodb://127.0.0.1:27017/datagraph');
