d4c11b4da670fed9d934c06ca557175ffda5e75d
Henra Setia Nugraha <henra.sn@gmail.com>
2016-07-29 23:29:48 +0700

    try fix produk3

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var graphHTTP = require('express-graphql');
-var graphql = require('graphql');
-var mongoose = require('mongoose');
-var Schema = require('./graph');
+// var express = require('express');
+// var graphHTTP = require('express-graphql');
+// var graphql = require('graphql');
+// var mongoose = require('mongoose');
+// var Schema = require('./graph');
 // var Model = require('./models/ratingModel').main;
 
 // var query = 'query{showSingleData(id:1234){completed,title}}';
 // var query = 'mutation mutationRate{add(idProduk:"32423"){}}';
 
-mongoose.connect('mongodb://dbrest:restdatabase@ds031965.mlab.com:31965/dbscript');
+// mongoose.connect('mongodb://dbrest:restdatabase@ds031965.mlab.com:31965/dbscript');
 // mongoose.connect('mongodb://127.0.0.1:27017/produk');
 // mongoose.connect('mongodb://127.0.0.1:27017/datagraph');
 
@@ -17,16 +17,16 @@ mongoose.connect('mongodb://dbrest:restdatabase@ds031965.mlab.com:31965/dbscript
 // });
 
 
-var app = express()
-  .use('/', graphHTTP({
-    schema: Schema,
-    pretty: true,
-    graphiql: true
-  }))
-  .listen(3001, (err) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('running');
-    }
-  })
+// var app = express()
+//   .use('/', graphHTTP({
+//     schema: Schema,
+//     pretty: true,
+//     graphiql: true
+//   }))
+//   .listen(3001, (err) => {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log('running');
+//     }
+//   })
